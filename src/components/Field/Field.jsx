a0afeb3142067cb16142d0ee7bcacc9a29da34b6
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-06-28 21:17:22 +0300

    Create Field.jsx

---
+++ src/components/Field/Field.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { Cell } from "../Cell/Cell";
+import { GameOwer } from "../Game/GameOwer";
+import { useInterval } from "../../hooks/useInterval";
+import { FIELD_ROW, DIRECTION } from "../../constants/constants";
+import { getCell, nextSnakePosition } from "../../utils//moveSnake";
+
+export const Field = () => {
+  const [snake, setSnake] = useState([{ x: 5, y: 5 }]);
+  const [direction, setDirection] = useState(DIRECTION.RIGHT);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+
+  const handleKeydown = (e) => {
+    switch (e.key) {
+      case "ArrowRight":
+        setDirection(DIRECTION.RIGHT);
+        break;
+      case "ArrowLeft":
+        setDirection(DIRECTION.LEFT);
+        break;
+      case "ArrowUp":
+        setDirection(DIRECTION.TOP);
+        break;
+      case "ArrowDown":
+        setDirection(DIRECTION.BOTTOM);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const [head, ...body] = snake;
+  const intersectsWithBody = body.some(
+    (item) => item.x === head.x && item.y === head.y
+  );
+
+  useInterval(
+    () => setSnake(nextSnakePosition(snake, direction)),
+    intersectsWithBody ? null : 500
+  );
+
+  return (
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
+      {intersectsWithBody ? (
+        <GameOwer />
+      ) : (
+        FIELD_ROW.map((x) => {
+          return (
+            <div key={x + "X"}>
+              {FIELD_ROW.map(
+                (y) => getCell(x, y, snake) || <Cell key={y + "Y"} />
+              )}
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+};
