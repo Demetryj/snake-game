202cd7a24ca00b039ce8abe8d133a37373511cf5
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-06-28 22:27:13 +0300

    add classes

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,11 +1,18 @@
 import clsx from "clsx";
 import css from "./Cell.module.css";
 
-export const Cell = ({ cellFood, cellSnake }) => {
+export const Cell = ({
+  cellFirstFood,
+  cellSecondFood,
+  cellThirdFood,
+  cellSnake,
+}) => {
   return (
     <div
       className={clsx(css.cell, {
-        [css.cellFood]: cellFood,
+        [css.cellFirstFood]: cellFirstFood,
+        [css.cellSecondFood]: cellSecondFood,
+        [css.cellThirdFood]: cellThirdFood,
         [css.cellSnake]: cellSnake,
       })}
     ></div>
